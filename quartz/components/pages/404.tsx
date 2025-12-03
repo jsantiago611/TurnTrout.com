aec5496b686b6078337b231b718252a4940cc7f1
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2025-12-03 19:42:21 +0000

    Removing 404 page

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -11,17 +11,11 @@ const NotFound: QuartzComponent = () => {
           <h1>404</h1>
           <p>
             That page doesn’t exist. <br />
-            But don’t leave! There <br />
-            are other fish in the pond.
+            But don’t leave! <br />
           </p>
         </div>
 
-        <img
-          src="https://assets.turntrout.com/static/images/turntrout-art-transparent.avif"
-          id="trout-reading"
-          className="no-select"
-          alt="Alex in a trout costume, reading a book."
-        />
+      
       </div>
     </article>
   )
